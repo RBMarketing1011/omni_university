cde556449354e4579d60428c5298474154fa5842
RBMarketing1011 <rbmarketingandanalytics@gmail.com>
2023-12-29 09:34:17 -0500

    fixing fetch

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -3,8 +3,9 @@ import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react'
 //May need to remove the /api at end
 //set it to reducerPath: 'api' on the createApi variable
 const baseQuery = fetchBaseQuery({
-	baseUrl: `${ process.env.URL }/api`,
-	credentials: 'include'
+	baseUrl: 'http://localhost:5173/api',
+	credentials: 'include',
+	jsonContentType: 'application/json'
 })
 
 export const apiSlice = createApi({
