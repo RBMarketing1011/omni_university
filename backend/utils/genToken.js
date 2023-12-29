01ec6509702c1ee427ae1f737ccbc7c10a8c5603
RBMarketing1011 <rbmarketingandanalytics@gmail.com>
2023-12-29 10:59:58 -0500

    commit

--- backend/utils/genToken.js
+++ backend/utils/genToken.js
@@ -3,14 +3,14 @@ const Jwt = require('jsonwebtoken')
 const genToken = (res, userId) =>
 {
 	const token = Jwt.sign({ userId }, process.env.JWT_SECRET, {
-		expiresIn: '30d'
+		expiresIn: '1d'
 	})
 
 	cookieOptions = {
 		httpOnly: true,
 		secure: process.env.NODE_ENV !== 'development',
-		sameSite: 'strict',
-		maxAge: 30 * 24 * 60 * 60 * 1000
+		sameSite: false,
+		maxAge: 1 * 24 * 60 * 60 * 1000
 	}
 
 	res.cookie('jwtToken', token, cookieOptions)
