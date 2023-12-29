687a008a1d868ec355727b3f73bc075149061f61
RBMarketing1011 <rbmarketingandanalytics@gmail.com>
2023-12-29 10:20:05 -0500

    commit

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -3,7 +3,7 @@ import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react'
 //May need to remove the /api at end
 //set it to reducerPath: 'api' on the createApi variable
 const baseQuery = fetchBaseQuery({
-	baseUrl: 'https://omni-university.com/api',
+	baseUrl: 'http://localhost:5173/api',
 	credentials: 'include',
 	jsonContentType: 'application/json'
 })
