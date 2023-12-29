6aa96a2ba1948a00585b04d1e0fc44e58693101e
RBMarketing1011 <rbmarketingandanalytics@gmail.com>
2023-12-29 14:59:40 -0500

    commit

--- backend/app.js
+++ backend/app.js
@@ -74,7 +74,7 @@ if (process.env.NODE_ENV === 'production')
   const __dirname = path.resolve()
   app.use(express.static(path.join(__dirname, 'frontend', 'dist')))
 
-  app.get('*', (req, res) =>
+  app.get('/*', (req, res) =>
   {
     res.sendFile(__dirname, 'frontend', 'dist', 'index.html')
   })
